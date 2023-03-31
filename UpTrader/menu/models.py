b7b0bfe403425d06731c4d0122b2af1acf1dd205
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-31 08:39:52 +0300

    add model

--- UpTrader/menu/models.py
+++ UpTrader/menu/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Menu(models.Model):
+    title = models.CharField(max_length=200,
+                             unique=True,
+                             verbose_name='Menu title')
+    slug = models.SlugField(max_length=200, verbose_name='Menu slug')
+
+    def __str__(self):
+        return self.title
+
+
+class Item(models.Model):
+    title = models.CharField(max_length=200,
+                             unique=True,
+                             verbose_name='Item title')
+    slug = models.SlugField(max_length=200, verbose_name='Item slug')
+    menu = models.ForeignKey(Menu,
+                             blank=True,
+                             null=True,
+                             related_name='items',
+                             on_delete=models.CASCADE)
+    parent = models.ForeignKey('self',
+                               blank=True,
+                               null=True,
+                               related_name='childrens',
+                               on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
