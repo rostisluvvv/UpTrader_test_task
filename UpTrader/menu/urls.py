64d7ea74881482121eb9448104b1aa3dc937980f
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-04-01 11:21:17 +0300

    add new path

--- UpTrader/menu/urls.py
+++ UpTrader/menu/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'menu'
 
 urlpatterns = [
-    path('menu/', views.MenuPageView.as_view(), name='index'),
+    path('', views.index_menu_page, name='index'),
+    path('/<slug:slug>/', views.single_menu_page, name='menu'),
 ]
