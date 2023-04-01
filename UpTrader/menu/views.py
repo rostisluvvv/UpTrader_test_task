3a3c86741e8afd68c68548f8fc6e5e6fa06702b8
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-04-01 11:21:34 +0300

    add single_menu_page

--- UpTrader/menu/views.py
+++ UpTrader/menu/views.py
@@ -1,5 +1,19 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .models import Menu
 
 
-class MenuPageView(TemplateView):
+
+
+def index_menu_page(request):
+    list_menu = Menu.objects.all()
     template_name = 'menu/index.html'
+    return render(request, template_name, {'list_menu': list_menu})
+
+
+def single_menu_page(request, slug):
+    menu = get_object_or_404(Menu, slug=slug)
+    context = {'menu_name': menu.title}
+    template_name = 'menu/menu_single_page.html'
+    return render(request, template_name, context)
