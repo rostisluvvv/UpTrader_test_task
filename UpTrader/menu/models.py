8f63266c92916dc3db4ab9721d6ed939c67994ee
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-04-03 08:13:10 +0300

    add dockstring

--- UpTrader/menu/models.py
+++ UpTrader/menu/models.py
@@ -2,15 +2,38 @@ from django.db import models
 
 
 class Menu(models.Model):
+    """
+    Model for storing website menus.
+
+    Fields:
+    - title (CharField): title of the menu.
+    - slug (SlugField): unique identifier of the menu for URLs.
+
+    Methods:
+    - __str__(): returns the title of the menu as a string.
+    """
     title = models.CharField(max_length=200,
                              verbose_name='Menu title')
-    slug = models.SlugField(max_length=200, unique=True, verbose_name='Menu slug')
+    slug = models.SlugField(max_length=200,
+                            unique=True,
+                            verbose_name='Menu slug')
 
     def __str__(self):
         return self.title
 
 
 class Item(models.Model):
+    """
+    Model for storing menu items.
+
+    Fields:
+    - title (CharField): title of the menu item.
+    - menu (ForeignKey): reference to the menu that the item belongs to.
+    - parent (ForeignKey): reference to the parent item (if any).
+
+    Methods:
+    - __str__(): returns the title of the menu item as a string.
+    """
     title = models.CharField(max_length=200,
                              unique=True,
                              verbose_name='Item title')
@@ -24,8 +47,6 @@ class Item(models.Model):
                                null=True,
                                related_name='childrens',
                                on_delete=models.CASCADE)
-
-    url = models.CharField(max_length=150, verbose_name='Item url')
 
     def __str__(self):
         return self.title
--- UpTrader/menu/views.py
+++ UpTrader/menu/views.py
@@ -1,18 +1,40 @@
-from django.views.generic import TemplateView
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404
 
 from .models import Menu
 
 
-
-
 def index_menu_page(request):
+    """
+    A view function to display a list of all menus.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The HTTP response object with the rendered template and the list of menus.
+
+    """
     list_menu = Menu.objects.all()
     template_name = 'menu/index.html'
     return render(request, template_name, {'list_menu': list_menu})
 
 
 def single_menu_page(request, slug):
+    """
+    A view function to display the details of a single menu.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+        slug (str): The unique slug of the menu.
+
+    Returns:
+        HttpResponse: The HTTP response object with the rendered template
+        and the menu details.
+
+    Raises:
+        Http404: If the menu with the given slug does not exist.
+
+    """
     menu = get_object_or_404(Menu, slug=slug)
     context = {'menu_name': menu.title}
     template_name = 'menu/menu_single_page.html'
